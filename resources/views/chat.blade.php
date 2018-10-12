72bd6e249bd2eca7e5428f4a59df6dadf0749263
Melody Cuenco <mcuenco11@gmail.com>
2018-10-12 16:59:41 +0800

    updated

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -2,11 +2,16 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <h1>Chat room</h1>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                Chatroom
+                <span> @{{usersInRoom.length}}</span> 
+                </div>
                 <chat-log :messages="messages"></chat-log>
                 <chat-composer v-on:messagesent="addMessage"></chat-composer>
+                </div>
             </div>
         </div>
     </div>
